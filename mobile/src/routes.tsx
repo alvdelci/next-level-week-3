d95ca311af67fb6bdfbed571a733fdb9ec2ebc64
Delcimario <delcimariosantos@gmail.com>
2020-10-19 00:12:11 -0300

    nlw day 5

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -8,25 +8,44 @@ const { Navigator, Screen } = createStackNavigator();
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDatails from './pages/OrphanageDetails';
 
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'}}}>
                 <Screen
                     name="OrphanagesMap"
                     component={OrphanagesMap}
-                    options={{
-                        headerShown: true,
-                        headerStyle: { backgroundColor: '#15c3d6' },
-                        title: 'Localização dos Orfanatos',
-                        headerTitleStyle: { color: '#FFF', alignSelf: 'center' }
-                    }}
                 />
 
                 <Screen
                     name="OrphanageDetails"
                     component={OrphanageDatails}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title='Orfanato' />
+                    }}
+                />
+
+                <Screen
+                    name="SelectMapPosition"
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title='Selecione o Mapa' />
+                    }}
+                />
+
+                <Screen
+                    name="OrphanageData"
+                    component={OrphanageData}
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title='Adicione os Dados' />
+                    }}
                 />
             </Navigator>
         </NavigationContainer>
